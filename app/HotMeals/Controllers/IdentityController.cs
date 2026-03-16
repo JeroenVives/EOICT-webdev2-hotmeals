5603a5b98052981a655cc98fd3952c5ed1836a32
Jeroen Reinenbergh <jeroen.reinenbergh@vives.be>
2026-03-16 11:40:43 +0100

    added user registration

---
+++ app/HotMeals/Controllers/IdentityController.cs
@@ -0,0 +1,47 @@
+﻿using HotMeals.Data.School;
+using HotMeals.Models.Views;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotMeals.Controllers
+{
+    [Route("mvc/[controller]")]
+    public class IdentityController : Controller
+    {
+        private readonly ILogger<IdentityController> _logger;
+        private readonly SchoolContext _schoolContext;
+        private readonly UserManager<SchoolUser> _userManager;
+        private readonly SignInManager<SchoolUser> _signInManager;
+
+        public IdentityController(ILogger<IdentityController> logger, SchoolContext schoolContext, UserManager<SchoolUser> userManager, SignInManager<SchoolUser> signInManager)
+        {
+            _logger = logger;
+            _schoolContext = schoolContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [Route("register")]
+        public IActionResult RegisterGet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterPost(RegisterGetViewModel registerGetViewModel)
+        {
+            var identityResult = await _userManager.CreateAsync(
+                new SchoolUser {
+                    FirstName = registerGetViewModel.FirstName,
+                    LastName = registerGetViewModel.LastName,
+                    Email = registerGetViewModel.Email,
+                    UserName = registerGetViewModel.Email
+                },
+                registerGetViewModel.Password);
+            ViewBag.IdentityResult = identityResult;
+            return View();
+        }
+    }
+}
